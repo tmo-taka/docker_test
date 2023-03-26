3dc4930ff37a0a09f86debf4718c6c2901610c35
takahara tomoya <t.tkorock221@gmail.com>
2023-03-26 12:40:55 +0900

    フォーム作成

--- vite-project/src/context/Login.tsx
+++ vite-project/src/context/Login.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 export const Login = () => {
+    const [inputName, setInputName] = useState<string>('');
+
+    const inputForm = (event:React.ChangeEvent<HTMLInputElement>):void => {
+        const newValue:string = event.target.value;
+        setInputName(newValue);
+    }
     return(
-        <div>
-            こんにちは
-        </div>
+        <form className="container">
+            <div className="title">Name</div>
+            <input type="text" value={inputName} onChange={inputForm}/>
+            <input type="submit" value="ログイン" />
+        </form>
     )
 }
